faef1f924405a017356b64275149c6227c5a3c98
NatalieGrasser <grasser@strw.leidenuniv.nl>
2024-11-07 16:58:09 +0100

    fixing configuration

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name='atm_retrieval',
+    name='SP0829_retrieval',
     version='0.1.0',
-    description='A package for atmospheric retrieval',
-    author='Natalie Grasser',
+    description='Atmospheric retrieval of SSSPMJ0829-1309',
+    author='Natalie Grasser & Colby Malcolm',
     author_email='grasser@strw.leidenuniv.nl',
     packages=setuptools.find_packages(),
 )
